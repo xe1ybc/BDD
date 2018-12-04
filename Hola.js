8546d0fbd03e5a3ab83a06d2a4967de7714a1847
aiden-lin <42947752+aiden-lin@users.noreply.github.com>
2018-12-04 16:07:01 -0600

    practicas

---
+++ Hola.js
@@ -0,0 +1,7 @@
+﻿var http=require("http");
+var  manejador=function(solicitud, repuesta){
+	consola.log("Recibimos una nueva petición");
+	repuesta.final("Hola Mundo");
+}
+var servidor=http.createServer(manejador);
+Servidor.listen(8080);

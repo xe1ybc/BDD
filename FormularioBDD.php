1ba9c681ba5278a32f3e5b12bcb32351d18ab226
AliciaHernandezOlvera <42949959+AliciaHernandezOlvera@users.noreply.github.com>
2018-12-14 22:06:43 -0600

    index

---
+++ FormularioBDD.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>formulario</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body style="background-color: #F7F9F9;">
+
+	<div>
+		<center><img src="imag/pman.jpg" ></center>
+	</div><br><br>
+
+<center><form action="varReg.php" method="POST">
+	<input type="text" name="fmusuario" placeholder="correo / nombre de usuario" size="25"/><br>
+	<input type="password" name="fmcontrasena" placeholder="contraseña"/><br>
+	<button type="submit">registro</button>
+</form></center>
+
+</body>
+</html>
